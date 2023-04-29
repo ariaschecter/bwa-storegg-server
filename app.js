dacb68b22e4b955bb40be3df61d18c8c7ba6bdac
ariaschecter <aria.maulana.m@gmail.com>
2023-04-29 13:11:53 +0700

    Category Create Read done

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var dashboardRouter = require('./app/dashboard/router');
+var categoryRouter = require('./app/category/router');
 
 var app = express();
 
@@ -18,9 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/adminlte', express.static(path.join(__dirname, '/node_modules/admin-lte/')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', dashboardRouter);
+app.use('/category', categoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
