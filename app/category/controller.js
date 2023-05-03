3d8c722148e3d4f93fa0356f255426411986fc72
ariaschecter <aria.maulana.m@gmail.com>
2023-05-03 10:33:04 +0700

    Category Update Done

--- app/category/controller.js
+++ app/category/controller.js
@@ -4,7 +4,7 @@ module.exports = {
   index: async (req, res) => {
     try {
       const category = await Category.find();
-      res.render('admin/category/view_category', {category});
+      res.render('admin/category/view_category', { category });
     } catch (err) {
       console.log(err);
     }
@@ -25,7 +25,34 @@ module.exports = {
 
       res.redirect('/category');
     } catch (err) {
-      console.log(err)
+      console.log(err);
+    }
+  },
+
+  viewEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const category = await Category.findOne({ _id: id });
+
+      res.render('admin/category/edit', {
+        category,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  actionEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { name } = req.body;
+
+      const category = await Category.findOneAndUpdate({ _id: id }, { name });
+
+      res.redirect('/category');
+    } catch (err) {
+      console.log(err);
     }
   },
 };
