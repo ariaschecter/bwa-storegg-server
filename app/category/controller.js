c4532bf75a2d49922db0f871c03956246a052468
ariaschecter <aria.maulana.m@gmail.com>
2023-05-03 11:00:42 +0700

    Category + Notification Done

--- app/category/controller.js
+++ app/category/controller.js
@@ -3,17 +3,25 @@ const Category = require('./model');
 module.exports = {
   index: async (req, res) => {
     try {
+      const alertMessage = req.flash('alertMessage');
+      const alertStatus = req.flash('alertStatus');
+
+      const alert = { message: alertMessage, status: alertStatus };
       const category = await Category.find();
-      res.render('admin/category/view_category', { category });
+      res.render('admin/category/view_category', { category, alert });
     } catch (err) {
-      console.log(err);
+      req.flash('alertMessage', `${err.message}`);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/category');
     }
   },
   viewCreate: async (req, res) => {
     try {
       res.render('admin/category/create');
     } catch (err) {
-      console.log(err);
+      req.flash('alertMessage', `${err.message}`);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/category');
     }
   },
 
@@ -23,9 +31,14 @@ module.exports = {
       let category = await Category({ name });
       await category.save();
 
+      req.flash('alertMessage', 'Berhasil Tambah Kategori');
+      req.flash('alertStatus', 'success');
+
       res.redirect('/category');
     } catch (err) {
-      console.log(err);
+      req.flash('alertMessage', `${err.message}`);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/category');
     }
   },
 
@@ -39,7 +52,9 @@ module.exports = {
         category,
       });
     } catch (err) {
-      console.log(err);
+      req.flash('alertMessage', `${err.message}`);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/category');
     }
   },
 
@@ -48,11 +63,35 @@ module.exports = {
       const { id } = req.params;
       const { name } = req.body;
 
-      const category = await Category.findOneAndUpdate({ _id: id }, { name });
+      await Category.findOneAndUpdate({ _id: id }, { name });
+
+      req.flash('alertMessage', 'Berhasil Ubah Kategori');
+      req.flash('alertStatus', 'success');
 
       res.redirect('/category');
     } catch (err) {
-      console.log(err);
+      req.flash('alertMessage', `${err.message}`);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/category');
+    }
+  },
+
+  actionDelete: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      await Category.findOneAndRemove({
+        _id: id,
+      });
+
+      req.flash('alertMessage', 'Berhasil Hapus Kategori');
+      req.flash('alertStatus', 'success');
+
+      res.redirect('/category');
+    } catch (err) {
+      req.flash('alertMessage', `${err.message}`);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/category');
     }
   },
 };
