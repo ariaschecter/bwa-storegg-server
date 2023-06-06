dc89c9a6bb6f398268552303e3c144eb583a34e5
ariaschecter <aria.maulana.m@gmail.com>
2023-06-06 21:18:01 +0700

    [PATCH] API Get Profile Done

--- app/player/controller.js
+++ app/player/controller.js
@@ -198,4 +198,19 @@ module.exports = {
       res.status(500).json({ message: err.message || `Internal server error` });
     }
   },
+  profile: async (req, res) => {
+    try {
+      const player = {
+        id: req.player._id,
+        username: req.player.username,
+        email: req.player.email,
+        name: req.player.name,
+        phoneNumber: req.player.phoneNumber,
+        avatar: req.player.avatar,
+      };
+      res.status(200).json({ data: player });
+    } catch (err) {
+      res.status(500).json({ message: err.message || `Internal server error` });
+    }
+  },
 };
