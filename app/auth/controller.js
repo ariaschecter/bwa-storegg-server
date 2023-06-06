1df660e3520769180046c819c943751240e17c04
ariaschecter <aria.maulana.m@gmail.com>
2023-06-06 11:18:47 +0700

    [PATCH] API Signin Done

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -2,9 +2,11 @@ const Player = require('../player/model');
 const path = require('path');
 const fs = require('fs');
 const config = require('../../config');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
-  signup: async (req, res) => {
+  signup: async (req, res, next) => {
     try {
       const payload = req.body;
 
@@ -43,7 +45,7 @@ module.exports = {
                 fields: err.errors,
               });
             }
-            // next();
+            next();
           }
         });
       } else {
@@ -62,7 +64,49 @@ module.exports = {
           fields: err.errors,
         });
       }
-      // next();
+      next(err);
     }
+  },
+  signin: async (req, res, next) => {
+    const { email, password } = req.body;
+    Player.findOne({ email: email })
+      .then((player) => {
+        if (player) {
+          const checkPassword = bcrypt.compareSync(password, player.password);
+          if (checkPassword) {
+            const token = jwt.sign(
+              {
+                player: {
+                  id: player.id,
+                  username: player.username,
+                  email: player.email,
+                  name: player.name,
+                  phoneNumber: player.phoneNumber,
+                  avatar: player.avatar,
+                },
+              },
+              config.jwtKey
+            );
+
+            res.status(200).json({
+              data: { token },
+            });
+          } else {
+            res.status(403).json({
+              message: 'Password yang anda masukkan salah.',
+            });
+          }
+        } else {
+          res.status(403).json({
+            message: 'Email yang anda masukkan belum terdaftar.',
+          });
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: err.message || `Internal server error`,
+        });
+        next();
+      });
   },
 };
