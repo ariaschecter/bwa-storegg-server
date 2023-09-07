9825e956bbdb53fc78e13bacc1b958aeab624f3c
ariaschecter <aria.maulana.m@gmail.com>
2023-09-07 09:52:01 +0700

    [UPDATE] Add detail voucher and payment in that api

--- app/player/controller.js
+++ app/player/controller.js
@@ -29,12 +29,18 @@ module.exports = {
         .populate('nominals')
         .populate('user', '_id name phoneNumber');
 
-      if (!voucher) {
+      const payment = await Payment.find()
+        .populate('banks');
+
+      if (!voucher || !payment) {
         return res
           .status(404)
           .json({ message: 'Voucher game tidak ditemukan!' });
       }
-      res.status(200).json({ data: voucher });
+      res.status(200).json({ data: {
+        detail: voucher,
+        payment: payment
+      } });
     } catch (err) {
       res.status(500).json({ message: err.message || `Internal server error` });
     }
